f5c751895ee5abea0c401f6a6c6383ce16af0cce
kimjisoo <classy960516@gmail.com>
2019-07-04 17:37:08 +0900

    eomcs-java-project-05 완료

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App2.java
@@ -1,14 +1,40 @@
 package com.eomcs.lms;
-
+import java.util.Scanner;
 public class App2 {
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("이름: 홍길동");
-    System.out.println("이메일: hong@test.com");
-    System.out.println("암호: 1111");
-    System.out.println("사진: hong.png");
-    System.out.println("전화: 1111-2222");
-    System.out.println("가입일: 2019-01-01");
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.print("번호?");
+    String no = scan.nextLine();
+    
+    System.out.print("이름?");
+    String name = scan.nextLine();
+    
+    System.out.print("메일?");
+    String email = scan.nextLine();
+    
+    System.out.print("암호?");
+    String password = scan.nextLine();
+    
+    System.out.print("사진?");
+    String picture = scan.nextLine();
+    
+    System.out.print("폰번호?");
+    String phoneNum = scan.nextLine();
+    
+    System.out.print("가입일?");
+    String joinDate = scan.nextLine();
+    
+    
+    System.out.println();
+    
+    System.out.println("번호: " + no);
+    System.out.println("이름: " + name);
+    System.out.println("이메일: " + email);
+    System.out.println("암호: " + password);
+    System.out.println("사진: " + picture);
+    System.out.println("전화: " + phoneNum);
+    System.out.println("가입일: " + joinDate);
 
   }
 }
