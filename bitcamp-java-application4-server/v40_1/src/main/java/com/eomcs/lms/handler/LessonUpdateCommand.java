0093780a0bba17992f36e78ea0a5f41bcb55368a
kimjisoo <classy960516@gmail.com>
2019-08-23 15:07:15 +0900

    bitcamp-java-application4-server v40_1

---
+++ bitcamp-java-application4-server/v40_1/src/main/java/com/eomcs/lms/handler/LessonUpdateCommand.java
@@ -0,0 +1,51 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.util.Input;
+
+public class LessonUpdateCommand implements Command {
+
+  private LessonDao lessonDao;
+
+  public LessonUpdateCommand(LessonDao lessonDao) {
+    this.lessonDao = lessonDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+
+    try {
+      int no = Input.getIntValue(in, out, "번호?");
+      Lesson lesson = lessonDao.findBy(no);
+      if (lesson == null) {
+        System.out.println("해당 수업을 찾을 수 없습니다.");
+        return;
+      }
+
+
+      String str = Input.getStringValue(in, out, "수업명");
+      if (str.length() > 0) {
+        lesson.setTitle(str);
+        lessonDao.update(lesson);
+        out.println("데이터 수정완료");
+
+      } else {
+        out.println("데이터 변경을 취소합니다.");
+      }
+
+
+
+    } catch (Exception e) {
+      System.out.println("데이터 수정 실패");
+      System.out.println(e.getMessage());
+
+    }
+
+  }
+
+}
+
+
