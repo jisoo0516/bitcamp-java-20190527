fd7fefe075c7f6796ed29e1cd58c5cf32ace8315
kimjisoo <classy960516@gmail.com>
2019-09-18 10:48:27 +0900

    bitcamp-java-application4-server v56_2

---
+++ bitcamp-java-application4-server/v56_2/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,14 @@
+// v56_2 : 리스너를 이용하여 스프링 IoC 컨테이너 준비하기
+package com.eomcs.lms;
+
+// 작업:
+// => ContextLoaderListener 리스너 생성
+// => AppInitServlet의 역할을 ContextLoaderListener로 이전
+// => web.xml 변경
+// => AppInitServlet 삭제
+
+// 설명위해 만든 클래스임
+
+public class App {
+
+}
